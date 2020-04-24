f3aae33f412f20a42f0ca03aa0aa5c560182311f
Antonio Bustorff <abustorff@infinera.com>
2020-04-24 12:50:53 +0100

    Include Favorite child items even if those are not marked as favorites.

--- src/main/java/hudson/plugins/favorite/filter/FavoriteFilter.java
+++ src/main/java/hudson/plugins/favorite/filter/FavoriteFilter.java
@@ -2,28 +2,45 @@ package hudson.plugins.favorite.filter;
 
 
 import hudson.model.Hudson;
+import hudson.model.Item;
+import hudson.model.ItemGroup;
 import hudson.model.TopLevelItem;
 import hudson.model.User;
 import hudson.model.View;
 import hudson.plugins.favorite.Favorites;
-import hudson.plugins.favorite.user.FavoriteUserProperty;
 import hudson.views.ViewJobFilter;
 import jenkins.model.Jenkins;
 import org.acegisecurity.Authentication;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class FavoriteFilter extends ViewJobFilter {
+
+    private boolean recursive;
+
     @DataBoundConstructor
     public FavoriteFilter() {
 
     }
 
+    public boolean isRecursive() {
+        return recursive;
+    }
+
+    @DataBoundSetter
+    public void setRecursive(boolean recursive) {
+        this.recursive = recursive;
+    }
+
+
     @Override
     public List<TopLevelItem> filter(List<TopLevelItem> added, List<TopLevelItem> all, View filteringView) {
-        List<TopLevelItem> filtered = new ArrayList<TopLevelItem>();
+        final Set<TopLevelItem> filtered = new HashSet<>();
 
         Authentication authentication = Hudson.getAuthentication();
 
@@ -38,11 +55,18 @@ public class FavoriteFilter extends ViewJobFilter {
                 for (TopLevelItem item : added) {
                     if (Favorites.isFavorite(user, item)) {
                         filtered.add(item);
+                        if (recursive && item instanceof ItemGroup) {
+                            for (Item subItem : ((ItemGroup<? extends Item>) item).getItems()) {
+                                if (subItem instanceof TopLevelItem) {
+                                    filtered.add((TopLevelItem) subItem);
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-        return filtered;
+        return new ArrayList<>(filtered);
     }
 
 }
